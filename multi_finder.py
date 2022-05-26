086c16d1c675c43704e873083274790185fcafb5
Lucas Wilson <lkwilson96@gmail.com>
2022-05-25 22:53:16 -0600

    cleanup multi

--- multi_finder.py
+++ multi_finder.py
@@ -3,21 +3,14 @@ import struct
 
 MCAST_GRP = '224.1.1.1'
 MCAST_PORT = 5007
-IS_ALL_GROUPS = True
+bind_addr = '192.168.1.11'
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_IP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-if IS_ALL_GROUPS:
-    # on this port, receives ALL multicast groups
-    sock.bind(('', MCAST_PORT))
-else:
-    # on this port, listen ONLY to MCAST_GRP
-    sock.bind((MCAST_GRP, MCAST_PORT))
+sock.bind((MCAST_GRP, MCAST_PORT))
 mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 while True:
-  # For Python 3, change next line to "print(sock.recv(10240))"
-  print(sock.recv(10240))
-
+  print(sock.recv(10240).decode('utf-8'))
