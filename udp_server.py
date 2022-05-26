ffd2ec52720ab860abdf5b38735bb8ad4cbe5789
Lucas Wilson <lkwilson96@gmail.com>
2022-05-25 22:55:23 -0600

    Servers need reuse

--- udp_server.py
+++ udp_server.py
@@ -5,7 +5,7 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
 
 # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 # sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 sock.bind(("192.168.1.255", 12345))
 while True:
