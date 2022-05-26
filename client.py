0feb5b2b2bc0e8834961b39caae2f523e30ebc0b
Lucas Wilson <lkwilson96@gmail.com>
2022-05-25 20:58:54 -0600

    Some example I found

--- client.py
+++ client.py
@@ -1,9 +1,25 @@
+#!/usr/bin/env python3
+
 import socket
+import time
 
-UDP_IP = "127.255.255.255"
-UDP_PORT = 5005
-MESSAGE = b"Hello, World!"
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+# Enable port reusage so we will be able to run multiple clients and servers on single (host, port).
+# Do not use socket.SO_REUSEADDR except you using linux(kernel<3.9): goto https://stackoverflow.com/questions/14388706/how-do-so-reuseaddr-and-so-reuseport-differ for more information.
+# For linux hosts all sockets that want to share the same address and port combination must belong to processes that share the same effective user ID!
+# So, on linux(kernel>=3.9) you have to run multiple servers and clients under one user to share the same (host, port).
+# Thanks to @stevenreddie
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+# Enable broadcasting mode
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+# Set a timeout so the socket does not block
+# indefinitely when trying to receive data.
+server.settimeout(0.2)
+message = b"your very important message"
+while True:
+    server.sendto(message, ("localhost", 37020))
+    print("message sent!", flush=True)
+    time.sleep(1)
